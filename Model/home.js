e61d089d73c18028dd5343886e61ccc59fdae281
MatteoSacchetti <matteo.sacchetti4@studio.unibo.it>
2024-08-09 23:01:08 +0200

    creata home con scaricamento asincrono dei post

---
+++ Model/home.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+    $.ajax({
+        url: '../Model/session.php',
+        method: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            if (response.email) {
+                downlaodPosts(response.email);
+            }
+        },
+        error: function (status, error) {
+            console.log('Error', status, error);
+        }
+    });
+});
+
+function downlaodPosts(email) {
+    $.ajax({
+        url: '../Model/home.php',
+        method: 'GET',
+        dataType: 'json',
+        success: function (posts) {
+            // Per ogni post che ottengo creo una struttura html
+            posts.forEach(function (post, _) {
+                // Se il post è il mio non lo mostro
+                if (post.email === email) {
+                    return;
+                }
+
+                // Creo struttura carousel di bootstrap per le immagini
+                let postHtml = `
+                    <div>
+                        <a href="profile.html?email=${post.email}" class="btn p-0"><h3>${post.name} ${post.surname}</h3></a>
+                        <div id="carousel-${post.post_id}" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                `;
+
+                // Aggiungo gli elementi del carousel
+                post.photo_urls.forEach(function (photo, index) {
+                    postHtml += `
+                        <div class="carousel-item ${index === 0 ? 'active' : ''}">
+                            <img src="${photo}" class="d-block w-100" alt="Slide ${index}">
+                        </div>
+                    `;
+                });
+
+                postHtml += `
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carousel-${post.post_id}" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carousel-${post.post_id}" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                        <p class="mt-3">${post.description}</p>
+                        <hr>
+                `;
+
+                // Aggiungo i commenti al post
+                post.comments.forEach(function (comment) {
+                    postHtml += `
+                        <div class="m-4">
+                            <a href="profile.html?email=${comment.author}" class="btn p-0"><h5>${comment.author_name} ${comment.author_surname}</h5></a>
+                            <p>${comment.comment}</p>
+                            <hr>
+                        </div>
+                    `;
+                });
+
+                postHtml += `
+                    </div>
+                    <hr class="mt-5 mb-5">
+                `;
+
+                $('#posts').append(postHtml);
+            });
+        },
+        error: function (status, error) {
+            console.log('Error', status, error);
+        }
+    });
+}
--- Model/home.php
+++ Model/home.php
@@ -6,40 +6,84 @@ session_start();
 require_once "../Database/database.php";
 $db = new database();
 
-// Query per scaricare tutti i post
+// Creo un array vuoto per memorizzare tutti i post
+$posts = [];
+
+// Scarico tutti i post
 $query = "
-        SELECT 
-            post.post_id,
-            post.description,
-            profilo.email,
-            profilo.nome,
-            profilo.cognome,
-            post_photos.photo_url
-        FROM post
-        JOIN profilo ON post.author = profilo.email
-        LEFT JOIN post_photos ON post.post_id = post_photos.post_id
-    ";
+    SELECT 
+        post.post_id,
+        post.description,
+        profilo.email,
+        profilo.nome,
+        profilo.cognome
+    FROM post
+    JOIN profilo ON post.author = profilo.email
+";
 $stmt = $db->sql->prepare($query);
 $stmt->execute();
-$stmt->bind_result($post_id, $description, $email, $name, $surname, $photos_url);
-$posts = [];
-$postIndex = [];
-
-// Salvo tutte le informazioni dei posts e le stampo
+$stmt->bind_result($post_id, $description, $email, $name, $surname);
 while ($stmt->fetch()) {
-    if (!isset($postIndex[$post_id])) {
-        $postIndex[$post_id] = count($posts);
-        $posts[] = [
-            'post_id' => $post_id,
-            'description' => $description,
-            'email' => $email,
-            'name' => $name,
-            'surname' => $surname,
-            'photo_urls' => []
-        ];
+    $posts[] = [
+        'post_id' => $post_id,
+        'description' => $description,
+        'email' => $email,
+        'name' => $name,
+        'surname' => $surname,
+        'photo_urls' => [],
+        'comments' => []
+    ];
+}
+$stmt->close();
+
+// Scarico tutte le foto
+$query = "
+    SELECT 
+        post.post_id,
+        post_photos.photo_url
+    FROM post
+    JOIN post_photos ON post.post_id = post_photos.post_id
+    ORDER BY post_photos.photo_url ASC
+";
+$stmt = $db->sql->prepare($query);
+$stmt->execute();
+$stmt->bind_result($post_id, $photos_url);
+while ($stmt->fetch()) {
+    foreach ($posts as &$post) {
+        if ($post['post_id'] == $post_id) {
+            $post['photo_urls'][] = $photos_url;
+            break;
+        }
     }
-    if ($photos_url !== null) {
-        $posts[$postIndex[$post_id]]['photo_urls'][] = $photos_url;
+}
+$stmt->close();
+
+// Scarico tutti i commenti
+$query = "
+    SELECT 
+        post.post_id,
+        post_comments.author,
+        profilo.nome,
+        profilo.cognome,
+        post_comments.comment
+    FROM post
+    JOIN post_comments ON post.post_id = post_comments.post_id
+    JOIN profilo ON post_comments.author = profilo.email
+";
+$stmt = $db->sql->prepare($query);
+$stmt->execute();
+$stmt->bind_result($post_id, $author, $author_name, $author_surname, $comment);
+while ($stmt->fetch()) {
+    foreach ($posts as &$post) {
+        if ($post['post_id'] == $post_id) {
+            $post['comments'][] = [
+                'author' => $author,
+                'author_name' => $author_name,
+                'author_surname' => $author_surname,
+                'comment' => $comment
+            ];
+            break;
+        }
     }
 }
 $stmt->close();
