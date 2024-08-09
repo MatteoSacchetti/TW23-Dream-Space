c176a9c5f95dfd661cbb90ec94df4453213ab284
MatteoSacchetti <matteo.sacchetti4@studio.unibo.it>
2024-08-09 22:57:46 +0200

    aggiunto nome e cognome al register

--- Model/register.php
+++ Model/register.php
@@ -1,17 +1,21 @@
 <?php
 
+session_start();
+
+$name = $_POST["name"];
+$surname = $_POST["surname"];
 $email = $_POST["email"];
 $password = $_POST["password"];
 
-if (isset($email) && isset($password)) {
+if (isset($email) && isset($password) && isset($name) && isset($surname)) {
     // Richiamo la classe Database
     require_once "../Database/database.php";
     $db = new database();
 
     // Query per inserire i dati nel database
-    $query = "insert into profilo (email, password) values (?, ?)";
+    $query = "insert into profilo (email, password, nome, cognome) values (?, ?, ?, ?)";
     $stmt = $db->sql->prepare($query);
-    $stmt->bind_param("ss", $email, $password);
+    $stmt->bind_param("ssss", $email, $password, $name, $surname);
     $stmt->execute();
     if ($stmt->affected_rows > 0) {
         $_SESSION["email"] = $email;
