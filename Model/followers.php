65f8748ac82307a40314a1735c47cc297fabe872
MatteoSacchetti <matteo.sacchetti4@studio.unibo.it>
2024-08-13 14:09:12 +0200

    chiamate php per vedere chi mi segue e chi seguo

--- Model/followers.php
+++ Model/followers.php
@@ -9,6 +9,29 @@ if (isset($_GET["email"])) {
     require_once "../Database/database.php";
     $db = new database();
 
-    // TODO Query per scaricare i followers di quella persona
+    // Query per scaricare i followers di quella persona
+    $query = "
+        SELECT 
+            profilo.nome,
+            profilo.cognome,
+            followers.author
+        FROM followers
+        JOIN profilo ON followers.author = profilo.email
+        WHERE followers.follower = ?
+    ";
+    $stmt = $db->sql->prepare($query);
+    $stmt->bind_param("s", $get_email);
+    $stmt->execute();
+    $stmt->bind_result($name, $surname, $author);
+    $followers = [];
+    while ($stmt->fetch()) {
+        $followers[] = [
+            "author" => $author,
+            "name" => $name,
+            "surname" => $surname
+        ];
+    }
+    $stmt->close();
 
+    echo json_encode($followers);
 }
--- Model/following.php
+++ Model/following.php
@@ -9,6 +9,29 @@ if (isset($_GET["email"])) {
     require_once "../Database/database.php";
     $db = new database();
 
-    // TODO Query per scaricare i followers di quella persona
+    // Query per scaricare i followers di quella persona
+    $query = "
+        SELECT 
+            profilo.nome,
+            profilo.cognome,
+            followers.follower
+        FROM followers
+        JOIN profilo ON followers.follower = profilo.email
+        WHERE followers.author = ?
+    ";
+    $stmt = $db->sql->prepare($query);
+    $stmt->bind_param("s", $get_email);
+    $stmt->execute();
+    $stmt->bind_result($name, $surname, $follower);
+    $following = [];
+    while ($stmt->fetch()) {
+        $following[] = [
+            "follower" => $follower,
+            "name" => $name,
+            "surname" => $surname
+        ];
+    }
+    $stmt->close();
 
+    echo json_encode($following);
 }
