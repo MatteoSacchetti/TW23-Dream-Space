a2520b5ef5d8ce5603e4661799cf96a5e0e1e7c8
MatteoSacchetti <matteo.sacchetti4@studio.unibo.it>
2024-08-14 13:23:20 +0200

    completata chiamata per vedere followers e following di ogni utente

--- Model/followers.js
+++ Model/followers.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const email = urlParams.get('email');
     $.ajax({
         url: '../Model/session.php',
         method: 'GET',
@@ -6,6 +9,7 @@ $(document).ready(function () {
         success: function (response) {
             if (response.email) {
                 $('#profilo').attr('href', 'profile.html?email=' + response.email);
+                downlaodFollowers(email);
             } else {
                 window.location.href = 'home.html';
             }
@@ -15,3 +19,25 @@ $(document).ready(function () {
         }
     });
 });
+
+function downlaodFollowers($getEmail) {
+    $.ajax({
+        url: '../Model/followers.php?email=' + $getEmail,
+        method: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            response.forEach(element => {
+                let followersHtml = `
+                    <div class="m-4">
+                        <a href="profile.html?email=${element.author}" class="btn p-0"><h5>${element.name} ${element.surname}</h5></a>
+                        <hr>
+                    </div>
+                `;
+                $('#followers').append(followersHtml);
+            });
+        },
+        error: function (status, error) {
+            console.log('Error', status, error);
+        }
+    });
+}
--- Model/following.js
+++ Model/following.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const email = urlParams.get('email');
     $.ajax({
         url: '../Model/session.php',
         method: 'GET',
@@ -6,6 +9,7 @@ $(document).ready(function () {
         success: function (response) {
             if (response.email) {
                 $('#profilo').attr('href', 'profile.html?email=' + response.email);
+                downlaodFollowing(response.email, email);
             } else {
                 window.location.href = 'home.html';
             }
@@ -15,3 +19,25 @@ $(document).ready(function () {
         }
     });
 });
+
+function downlaodFollowing($sessionEmail, $getEmail) {
+    $.ajax({
+        url: '../Model/following.php?email=' + $getEmail,
+        method: 'GET',
+        dataType: 'json',
+        success: function (response) {
+            response.forEach(element => {
+                let followingHtml = `
+                    <div class="m-4">
+                        <a href="profile.html?email=${element.follower}" class="btn p-0"><h5>${element.name} ${element.surname}</h5></a>
+                        <hr>
+                    </div>
+                `;
+                $('#following').append(followingHtml);
+            });
+        },
+        error: function (status, error) {
+            console.log('Error', status, error);
+        }
+    });
+}
